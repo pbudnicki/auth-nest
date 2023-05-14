017a52be17f3849a0510f50be1ffe056f0fa3a72
Patryk.budnicki <patryk.budnicki@siili.com>
2023-05-14 11:33:22 +0200

    feat: invalidating refresh tokens

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -11,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthenticationGuard } from './authentication/guards/authentication.guard';
 import { AccessTokenGuard } from './authentication/guards/access-token.guard';
+import { RefreshTokenIdsStorage } from './authentication/refresh-token-ids.storage';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { AccessTokenGuard } from './authentication/guards/access-token.guard';
       useClass: AuthenticationGuard,
     },
     AccessTokenGuard,
+    RefreshTokenIdsStorage,
     AuthenticationService,
   ],
   controllers: [AuthenticationController],
